03acc1409d581e4270ffc2f0ed9868dd0f949945
Drobov Vladimir <vladimir.drobov@gmail.com>
2019-02-20 17:36:29 +0200

    Lesson#8_Ex17

--- src/test/java/tests/MyListsTests.java
+++ src/test/java/tests/MyListsTests.java
@@ -76,4 +76,98 @@ public class MyListsTests extends CoreTestCase
 
         MyListsPageObject.swipeByArticleToDelete(article_title);
     }
+
+    @Test
+    public void testSaveTwoArticlesToMyList()
+    {
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
+
+        ArticlePageObject.hideBanner();
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine(search_line);
+        SearchPageObject.clickByArticleWithSubstring("bject-oriented programming language");
+
+        ArticlePageObject.hideBanner();
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        if (Platform.getInstance().isAndroid()){
+            String name_of_folder = "Folder";
+            ArticlePageObject.clickAddArticleToMyList();
+            ArticlePageObject.fillMyListName(name_of_folder);
+            ArticlePageObject.moveToSearchPage();
+            SearchPageObject.clickByRecentSearchResultWithSubstring(search_line);
+        } else {
+            ArticlePageObject.hideBanner();
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        if (Platform.getInstance().isIOS()) {
+            ArticlePageObject.closeSyncPopup();
+            ArticlePageObject.moveToSearchPage();
+        }
+
+        if (Platform.getInstance().isMW()){
+            AuthorizationPageObject Auth = new AuthorizationPageObject(driver);
+            Auth.clickAuthButton();
+            Auth.enterLoginData(login,password);
+            Auth.submitForm();
+
+            ArticlePageObject.waitForTitleElement();
+
+            assertEquals(
+                    "We are not on the same page after login",
+                    article_title,
+                    ArticlePageObject.getArticleTitle());
+
+            ArticlePageObject.addArticlesToMySaved();
+            ArticlePageObject.moveToSearchPage();
+            SearchPageObject.initSearchInput();
+            SearchPageObject.typeSearchLine(search_line);
+        }
+
+        SearchPageObject.waitForSearchResult("sland of Indonesia");
+        SearchPageObject.clickByArticleWithSubstring("sland of Indonesia");
+
+        ArticlePageObject.waitForTitleElement();
+
+        if (Platform.getInstance().isAndroid()){
+            ArticlePageObject.clickAddArticleToMyList();
+            ArticlePageObject.clickByFolderWithSubstring(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        ArticlePageObject.closeArticle();
+
+        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
+        NavigationUI.openNavigation();
+        NavigationUI.clickMyLists();
+
+        MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+
+        if (Platform.getInstance().isAndroid()){
+            MyListsPageObject.openFolderByName(name_of_folder);
+        }
+
+        List<String> listOfTitlesBeforeDelete = MyListsPageObject.getArticlesList();
+
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+        MyListsPageObject.waitForArticleToDisappearByTitle(article_title);
+
+        List<String> listOfTitlesAfterDelete = MyListsPageObject.getArticlesList();
+
+        assertEquals(
+                "Number of articles is wrong",
+                listOfTitlesAfterDelete.size(),
+                1
+        );
+
+        assertEquals(
+                "Number of articles is wrong",
+                listOfTitlesAfterDelete.get(0),
+                listOfTitlesBeforeDelete.get(0)
+        );
+    }
 }
